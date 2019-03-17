9de6ae9e32d33d50ffc8039e402e293063debde2
Philippe Brais <philippe.brais@polymtl.ca>
2019-03-16 22:28:12 -0400

    Limiter le PID Output Range
    Pour ne pas trop forcer la mécanique.

--- src/main/cpp/subsystems/Bras.cpp
+++ src/main/cpp/subsystems/Bras.cpp
@@ -89,8 +89,8 @@ void sysBras::EnablePID(double k_p, double k_i, double k_d, double k_f)
 
 	m_pidController->SetPID(k_p, k_i, k_d, k_f);
 	m_pidController->SetSetpoint(m_encoder.GetDistance());
-	m_pidController->SetOutputRange(-1.0, 1.0); // rad
 	m_pidController->SetInputRange( -1.2, 0.0); // rad
+	m_pidController->SetOutputRange(-0.5, 0.5); // rad
 	m_pidController->Reset();
 	Enable();
 }
