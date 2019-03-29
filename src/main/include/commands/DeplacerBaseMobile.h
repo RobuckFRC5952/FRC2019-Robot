4f7a0d4974a0c34da47174352f3910e12097a8d6
Philippe Brais <philippe.brais@polymtl.ca>
2019-03-28 20:11:53 -0400

    Implanter la commande DeplacerBaseMobile
    Même logique que DeplacerBras.

---
+++ src/main/include/commands/DeplacerBaseMobile.h
@@ -0,0 +1,27 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2017-2018 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+#pragma once
+
+#include "cinematique/DeplaceMoteur.h"
+
+#include <wpi/Logger.h>
+
+class cmdDeplacerBaseMobile : public cmdDeplaceMoteur
+{
+ public:
+	explicit cmdDeplacerBaseMobile(double distance);	// TODO Vitesse initiale et finale
+	virtual void Initialize() override;
+	virtual void Execute() override;
+	virtual bool IsFinished() override;
+	virtual void End() override;
+	virtual void Interrupted() override;
+
+ private:
+	/// La distance à parcourir.
+	double m_distance;
+};
